629deb33b524a8caf9551837dca92ee83738c6a0
SAVI <87853311+Blacksavi@users.noreply.github.com>
2025-03-04 11:33:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=heFknCRJ#n9wepUcXe
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94770377817' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
-POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://dbmasteruser:6f%25pG%3C9P,)%yGopWFh%7C}%2An%23,Z%25Geq(,r6@ls-f8f13eaa01e8449cf926550c14e4a1740b175578.czw8mu4c671v.us-east-1.rds.amazonaws.com:5432/blacksavi' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
 MODE: process.env.MODE === undefined ? 'nonbutton' : process.env.MODE,
 STATUS_REPLY_MESSAGE: '',    
